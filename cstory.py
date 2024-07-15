165fb4847c93f0a986556366b8929870ed49a3fc
Junior Ohanyere <junohanyere@gmail.com>
2024-07-15 10:14:01 +0100

    entry point module

---
+++ cstory.py
@@ -0,0 +1,41 @@
+"""Chat story module.
+"""
+
+import curses
+
+
+class ChatStory:
+    def __init__(self, title, desc):
+        """Initialize self. See help(type(self)) for accurate signature.
+        """
+
+        self._title = title
+        self._desc = desc
+
+        self._screen = None
+
+    def _prestart(self, screen):
+        """Set up screen properties.
+        """
+
+        self._screen = screen
+
+        curses.curs_set(0)
+
+    def title(self):
+        """Return the title of the chat story.
+        """
+
+        return (self._title)
+
+    def description(self):
+        """Return the description of the chat story.
+        """
+
+        return self._desc
+
+    def run(self):
+        """Run the Chat story application.
+        """
+
+        curses.wrapper(self._prestart)
