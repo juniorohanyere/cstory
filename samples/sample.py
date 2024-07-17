8dfda32aab864e290151c17fcecc49b785a66a0a
Junior Ohanyere <junohanyere@gmail.com>
2024-07-17 18:32:34 +0100

    fixed pycodestyle errors

--- samples/sample.py
+++ samples/sample.py
@@ -1,6 +1,7 @@
 from .. import cstory
 from .. import elem
 
+
 class Story(cstory.ChatStory):
     def __init__(self, title, desc):
         super().__init__(title, desc)
@@ -45,6 +46,7 @@ class Story(cstory.ChatStory):
         elem1.echo(msg='Hi babe')
         elem.echo(msg='Hi friend')
         elem1.echo(msg='Hi babe')
+
 
 cs = Story('ChatStory', 'A simple chat story')
 cs.run()
